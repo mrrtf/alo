9b0f54466444516716e439c9e421d66a33829372
dstocco <diego.stocco@cern.ch>
2017-11-13 14:31:53 +0100

    Whitespace changes

--- r23/O2Muon.h
+++ r23/O2Muon.h
@@ -14,21 +14,21 @@ class O2Muon : public TObject
 public:
   O2Muon(const char* ocdbpath="local:///Users/laurent/Alice/OCDBcopy2011");
   virtual ~O2Muon();
-  
+
   /** simple loop over events with decoding of the MCH data */
   int decodeEvents(const char* rawDataInputFile);
 
   /** Create a new raw data file with only the events matching the given trigger.
    */
   int filterRaw(const char* rawDataInputFile="/alice/data/2011/LHC11h/000169099/raw/11000169099001.28.FILTER_RAWMUON.root", const char* triggerClass="CPBI2_B1-B-NOPF-ALLNOTRD");
-  
+
   /** Create a Root file with (calibrated) MCH digits from a raw data file.
-    * Not meant to be fast, just a re-use of the existing classes to get the job done. 
+    * Not meant to be fast, just a re-use of the existing classes to get the job done.
     */
   int makeDigitFile(const char* rawDataInputFile="/alice/data/2011/LHC11h/000169099/raw/11000169099001.28.FILTER_RAWMUON.root", const char* digitOutputFile="digits.root", Bool_t calibrate=kTRUE);
 
   int makeDigitFiles(const char* rawDataInputFileList, const char* triggerClass="", Bool_t calibrate=kTRUE);
-  
+
   /// Make pre-clusters out of digits
   int makeClustering(const char* digitInputFile="digits.root",
                      const char* clusterOutputFile="clusters.root",
@@ -55,10 +55,10 @@ private:
   void prepareOCDB(int runNumber, AliRawReader* rawReader=0x0);
 
   int setupMagneticField();
-  
+
 private:
   std::string mOCDBPath;
-  
+
   ClassDef(O2Muon,0)
 };
 
